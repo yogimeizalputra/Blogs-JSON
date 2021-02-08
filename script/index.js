8100e8bd6aecdf77c84b1e9a0e09d9f963915f27
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-02-09 02:49:55 +0700

    Adding Blogs Section

---
+++ script/index.js
@@ -0,0 +1,27 @@
+//javascript for index.html
+const container =document.querySelector('.blogs');
+
+const renderPosts = async () => {
+    let uri = 'http://localhost:3000/posts';
+
+    const response = await fetch(uri);
+    const posts = await response.json();
+
+    let template = '';
+    posts.forEach( post => {
+        template += 
+        `<div class="post">
+            <h2>${post.title}</h2>
+            <p><small>${post.likes} likes</small></p>
+            <p>${post.body.slice(0, 200)}</p>
+            <a href="details.html">read more..</a>
+        </div>`
+        
+    })
+
+    container.innerHTML = template;
+    
+}
+
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
